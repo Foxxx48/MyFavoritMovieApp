32bd1e02c2704d7c87e61ed7d3b82dbf26a07be6
Foxxx48 <Foxxdim@mail.ru>
2022-12-09 12:33:09 +0300

    Added dependencies in build.gradle(Module)

--- app/src/main/java/com/fox/myfavoritmovieapp/MainActivity.kt
+++ app/src/main/java/com/fox/myfavoritmovieapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.fox.myfavoritmovieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.fox.myfavoritmovieapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding ?: throw RuntimeException("ActivityMainBinding = null")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    fun myLog(message: Any?) {
+        Log.d(TAG, "Main Activity: $message")
+    }
+
+    companion object {
+        const val TAG = "myApp"
     }
 }
