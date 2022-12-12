d10042e00e6052927fd45a39b55c26fa75a626c3
Foxxx48 <Foxxdim@mail.ru>
2022-12-12 15:20:24 +0300

    Added buildUrl()

--- app/src/main/java/com/fox/myfavoritmovieapp/MainActivity.kt
+++ app/src/main/java/com/fox/myfavoritmovieapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.fox.myfavoritmovieapp.databinding.ActivityMainBinding
+import com.fox.myfavoritmovieapp.utils.NetworkUtils
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        myLog(NetworkUtils.buildURL(5))
     }
 
     override fun onDestroy() {
